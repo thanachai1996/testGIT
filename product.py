b5ebca3f1d798ae90e6eca0cba5418474893bfe5
thanachai1996 <thanachai.99928@gmail.com>
2019-03-25 18:04:06 +0700

    add new

--- product.py
+++ product.py
@@ -1,7 +1,26 @@
-n = 10
-sum = 0
-for i in range(1,1 + n):
-    sum = sum + i 
+# nums=[10,11,1,0.1,-1,99,+100,-0.1]
+# max = nums[0]
+# for n in nums:
+#     if n > max:
+#         max = n
+# min = nums[0]
+# for n in nums:
+#     if n < min:
+#         min = n
+# print("max velue = ",max )
+# print("min velue = ",min )
 
-print("Allsum velue = ",(n *(n+1)/ 2.0))
-print("sum velue = ",sum)
+# n = 100
+# sum = 0
+# for i in range(1,n+1):
+#     sum = sum + i
+# print("sum velue = ",sum)
+# print("sums velue  = ", (n * (n+1) / 2 ))
+
+n = int(input("Enter Number = "))
+for i in range(n,0,-1):
+    print(i)
+
+print("+++++++++++++++++++++++")
+for j in range(n):
+    print(j)
