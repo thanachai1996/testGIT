0a027e4f31dc55c274cd4667d9f78ed114b915c7
thanachai1996 <thanachai.99928@gmail.com>
2019-03-26 16:42:53 +0700

    add

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def Linegraph():
     plt.plot([1,2,3,4,9,7,1])
@@ -17,20 +17,54 @@ def Piechart():
     plt.pie(vel,labels=label)
     plt.show()
 
+def Mixedgraph_Y2():
+    labels = np.array(["A","B","C","D"])
+    x = np.arange(labels.size)
+    y1 = np.random.normal(800,90,labels.size)
+    fig, axl = plt.subplots()
+    axl.bar(x,y1)
+    axl.set_ylabel("ton")
+    axl.tick_params("y",color="green")
+
+
+    axl.set_xticks(x)
+    axl.set_xticklabels(labels)
+    axl.legend()
+    axl.set_title("grech")
+
+    ax2 = axl.twinx()
+    y2 = np.random.normal(800,90,labels.size)
+    ax2.plot(x, y2, marker="o" , color="green")
+    ax2.tick_params("y",color="green")
+    ax2.set_ylabel("profile",color="green" ,fontsize=18)
+    ax2.set_ylim(ymin=0)
+
+    plt.show()
+
+
+
+
+
+
+
+
+
 
 print("MANU")
 print("1. Linegraph")
 print("2. Bargraph")
 print("3. Piechart")
+print("4. Mixedgraph")
 
 n = int(input("Manu : "))
 if n == 1:
     print(Linegraph())
 elif n == 2:
     print(Bargraph())
-else:
+elif n == 3:
     print(Piechart())
-
+else:
+    print(Mixedgraph_Y2())
 
 
 
